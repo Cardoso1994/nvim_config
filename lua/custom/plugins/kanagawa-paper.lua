d40fc8a88f89d0f2970353d78953a7411bd2ab78
Marco Cardoso <marcoacardosom@gmail.com>
2025-10-07 00:05:44 -0600

    kanagawa-paper: add dim inactive

--- lua/custom/plugins/kanagawa-paper.lua
+++ lua/custom/plugins/kanagawa-paper.lua
@@ -5,13 +5,9 @@ return {
     priority = 1000, -- Load this before other colorschemes
     enabled = true,
     config = function()
-      -- require('kanagawa-paper').setup {
-      --   theme = 'wave',
-      --   background = {
-      --     dark = 'dragon',
-      --     light = 'lotus',
-      --   },
-      -- }
+      require('kanagawa-paper').setup {
+        dim_inactive = true,
+      }
       vim.cmd 'colorscheme kanagawa-paper'
     end,
   },
